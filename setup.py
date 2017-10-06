bb9d8748d48af33fe8cac1f4a8a4078394285644
Tom McClintock <tmcclintock89@gmail.com>
2017-10-06 15:13:08 -0700

    Added a setup file so that it's easy to get to the mass function data.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Aemulus_data',
+      version='1.0',
+      description='Access to the Aemulus data.',
+      author='Aemulus team.',
+      py_modules=['aemulus_data']
+)
