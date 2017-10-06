4a5f7c56415a6ce92b1d8b0919a35d96ac38191f
Tom McClintock <tmcclintock89@gmail.com>
2017-10-06 15:17:35 -0700

    The emulator data isn't being copied into the anaconda library area. Have to figure this out later.

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='Aemulus_data',
       version='1.0',
       description='Access to the Aemulus data.',
       author='Aemulus team.',
-      py_modules=['aemulus_data']
+      py_modules=['aemulus_data'],
 )
