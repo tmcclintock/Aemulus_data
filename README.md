f90ba6bf6b0e617c218864ea0912465f116f7840
Tom McClintock <tmcclintock89@gmail.com>
2017-11-23 20:54:44 -0700

    Made the tests runable from the top level.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Aemulus_data
 A collection of all the data (excluding particle catalogs) used in Aemulus.
 
+To install `python setup.py install`. To run the tests `python setup.py test`.
+
 For more information, see the [documentation](http://aemulus-data.readthedocs.io/en/latest/).
--- setup.py
+++ setup.py
@@ -6,12 +6,10 @@ setup(
     author = "Aemulus team.",
     description='Access to the Aemulus data.',
     py_modules=['aemulus_data'],
-    include_package_data = True,
-    
+    include_package_data = True,    
     # adding packages
     packages=find_packages(),
-    #package_dir = {'':''},
-    
-    # trying to add files...
-    #package_data = {'': ["building_box_cosmologies.txt"]}
+    install_requires=['numpy'],
+    setup_requires=['pytest_runner'],
+    tests_require=['pytest']
 )
